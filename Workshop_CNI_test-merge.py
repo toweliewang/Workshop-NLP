0b95bd6a333f0eddef91b03066eabb118c2358b6
Wang Zilong <46743553+toweliewang@users.noreply.github.com>
2020-02-02 01:15:48 +0800

    Google Dialogflow speech to text & text to speech

---
+++ Workshop_CNI_test-merge.py
@@ -0,0 +1,89 @@
+def detect_intent_audio(project_id, session_id, audio_file_path,
+                        language_code):
+    """Returns the result of detect intent with an audio file as input.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    import dialogflow_v2 as dialogflow
+
+    session_client = dialogflow.SessionsClient()
+
+    # Note: hard coding audio_encoding and sample_rate_hertz for simplicity.
+    audio_encoding = dialogflow.enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
+    sample_rate_hertz = 16000
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+
+    with open(audio_file_path, 'rb') as audio_file:
+        input_audio = audio_file.read()
+
+    audio_config = dialogflow.types.InputAudioConfig(
+        audio_encoding=audio_encoding, language_code=language_code,
+        sample_rate_hertz=sample_rate_hertz)
+    query_input = dialogflow.types.QueryInput(audio_config=audio_config)
+
+    response = session_client.detect_intent(
+        session=session, query_input=query_input,
+        input_audio=input_audio)
+
+    print('=' * 20)
+    print('Query text: {}'.format(response.query_result.query_text))
+    print('Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence))
+    text = response.query_result.fulfillment_text
+    print('Fulfillment text: {}\n'.format(
+        response.query_result.fulfillment_text))
+    return text
+
+def detect_intent_with_texttospeech_response(project_id, session_id, texts,
+                                             language_code):
+    """Returns the result of detect intent with texts as inputs and includes
+    the response in an audio format.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    import dialogflow_v2 as dialogflow
+    session_client = dialogflow.SessionsClient()
+
+    session_path = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session_path))
+
+    for text in texts:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        # Set the query parameters with sentiment analysis
+        output_audio_config = dialogflow.types.OutputAudioConfig(
+            audio_encoding=dialogflow.enums.OutputAudioEncoding
+            .OUTPUT_AUDIO_ENCODING_LINEAR_16)
+
+        response = session_client.detect_intent(
+            session=session_path, query_input=query_input,
+            output_audio_config=output_audio_config)
+
+        print('=' * 20)
+        print('Query text: {}'.format(response.query_result.query_text))
+        print('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        print('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+        # The response's audio_content is binary.
+        with open('output.wav', 'wb') as out:
+            out.write(response.output_audio)
+            print('Audio content written to file "output.wav"')
+
+import os
+json_file = "xxx.json'   # private service account 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_file
+text = detect_intent_audio('testagent-ycumqv','12345',	'book_a_room.wav', 'en')
+path = os.getcwd()+"\\TestAgent-b4f1d2524c59.json"
+detect_intent_with_texttospeech_response('testagent-ycumqv','12345', [text], 'en')
+
+
+
+
